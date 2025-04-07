f24cec3fe9b4210d5428505b588f009760cea8af
ujjwalmishra2050 <ujjwalmishra272@gmail.com>
2025-04-07 12:53:41 +0545

    Created Nav Page

---
+++ script.js
@@ -0,0 +1,14 @@
+let fblink = document.querySelector('.btn')
+fblink.addEventListener('click' , function () {
+ window.location.href='https://www.facebook.com/RSDCONCERN# '
+})
+
+let whatsaplink = document.querySelector('.btn2')
+whatsaplink.addEventListener('click' , function(){
+    window.location.href= 'https://api.whatsapp.com/send/?phone=9779808685497&text&type=phone_number&app_absent=0'
+})
+
+let imageButton = document.querySelector('.logobutton')
+imageButton.addEventListener('click' ,function(){
+    window.location.href= 'https://rsdconcern.com/'
+})
